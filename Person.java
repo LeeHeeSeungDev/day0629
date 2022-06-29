871dbc6e5b4c6b233b2f64291130a776bee1c7d2
LeeHeeSeungDev <ranfimon3150@naver.com>
2022-06-29 10:07:58 +0900

    Book, Person 생성자 추가 [이희승]

--- Person.java
+++ Person.java
@@ -1,7 +1,14 @@
 class  
 {
-	public static void main(String[] args) 
-	{
-		System.out.println("Hello person");
+	private String name;
+	private int age;
+	D
+	public Person(String name, int age){ 
+		this.name = name;
+		this.age = age;
+	}
+
+	public Person(){
+	
 	}
 }
